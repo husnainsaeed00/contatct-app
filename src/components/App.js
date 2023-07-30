751f881602ca34dfcf022554d1942ffb228b6eab
Husnain Saeed <husnainsaeed000@gmail.com>
2023-07-30 03:28:14 -0700

    First Day of that Contact App Project

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import Header from "./Header";
+import AddContact from './AddContact';
+import ContactList from "./ContactList";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
